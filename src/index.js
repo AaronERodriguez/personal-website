67b0c4f67d63fc47c96de6e2c9d67338c1e7ef14
Expak12 <69766780+Expak12@users.noreply.github.com>
2023-06-28 00:54:56 -0600

    Set up the Projects page template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Projects from './Pages/Projects';
+
+const router = createBrowserRouter([
+  {
+    path: "/personal-website",
+    element: <App />,
+  },
+  {
+    path: "projects",
+    element: <Projects />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
