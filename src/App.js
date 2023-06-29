7f5d88b79aa90255c387b3a5f1e643ccb46b4194
Expak12 <69766780+Expak12@users.noreply.github.com>
2023-06-28 19:47:06 -0600

    Added bootstrap and navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 function App() {
+  document.body.setAttribute("class", "dark")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="/projects">Projects</Link>
-      </header>
+      <Navbar />
+      <h1>Hi</h1>
     </div>
   );
 }
