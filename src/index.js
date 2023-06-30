1ccac02a2a5cdeb4025a6ac7d32b0003771e3aa8
Expak12 <69766780+Expak12@users.noreply.github.com>
2023-06-29 19:29:35 -0600

    added the dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
